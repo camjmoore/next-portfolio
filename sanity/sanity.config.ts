7c9c6fc142c2380aade4dfd98b98be3ac3b95e18
cam <cjmooredev@gmail.com>
2025-06-03 12:46:13 -0400

    moved sanity config files to /sanity

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -7,22 +7,22 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
+import {defineDocumentType} from '@/sanity/typed-document-types'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schemaTypes'
-import {structure} from './sanity/structure'
+import {apiVersion, dataset, projectId} from '@/sanity/env'
+import {schema} from '@/sanity/schemaTypes'
+import {structure} from '@/sanity/structure'
 
 export default defineConfig({
-  basePath: '/sanity-studio',
+  basePath: '/studio',
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-    structureTool({structure}),
+    structureTool(structure),
+    visionTool({defaultApiVersion: apiVersion}),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
   ],
 })
